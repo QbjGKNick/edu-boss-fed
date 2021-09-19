dd20a97698a635b2643d25386f5b4b58cfce112a
jiangqb <jiangqb@citycloud.com.cn>
2021-09-19 15:43:52 +0800

    feat(添加样式): 添加样式

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,59 @@
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class App extends Vue {
+  a = 1
+  b = 2
+  c = 3
+  test (): void {
+    this.a = 4
+  }
+}
+</script>
+
+<!-- <script lang="ts">
+import Vue from "vue"
+
+export default Vue.extend({
+  data () {
+    return {
+      a: 1,
+      b: 2,
+      c: 3
+
+    }
+  },
+  methods: {
+    test() {
+      this.a.abc()
+    }
+  }
+})
+</script> -->
+
+<!-- <script>
+// 1. 编辑器给的类型提示
+// 2. Typescript 编译期间的类型提示
+export default {
+  data () {
+    return {
+      a: 1,
+      b: 2,
+      c: 3
+
+    }
+  },
+  methods: {
+    test() {
+      this.a.abc()
+    }
+  }
+}
+</script> -->
+
 <style lang="scss" scoped></style>
